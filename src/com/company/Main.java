f88ecc5eb7ba97bc3e6ef51605141e05f9a83a35
qwaukulous <qwauknation@yahoo.com>
2019-04-17 14:16:10 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("what is your name?");
+        String myAnswer = keyboard.nextLine();
+    }
+}
